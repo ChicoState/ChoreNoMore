735a2cff800740985f7781ed9c584eb8c49a0871
Mikayla Billings-Alston <m.billings01@yahoo.com>
2023-02-20 22:05:17 -0800

    Added a basic way of adding chores

--- chore-no-more/src/App.js
+++ chore-no-more/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//https://www.robinwieruch.de/react-add-item-to-list/
+
+import React from "react"
+
+const initialChores = [
+  {
+    name: "Test",
+    date: "2023-02-20",
+    completed: true,
+    frequency: "Weekly",
+  },
+];
+
+const App = () => {
+  const [chores, setChores] = React.useState(initialChores);
+  const [name, setName] = React.useState('')
+  const [date, setDate] = React.useState('')
+  const [frequency, setFrequency] = React.useState('')
+  const [completed, setCompleted] = React.useState('')
+  const completedChores = chores.filter(item => item.completed === true);
+  const incompleteChores = chores.filter(item => item.completed === false);
+
+  function handleAdd () {
+    const newList = chores.concat({ name, date, completed, frequency });
+    setChores(newList);
+    console.log(name, date, completed)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add Chores</h1>
+      <p>
+        <input placeholder="Chore Name" value={name} onChange={e => setName(e.target.value)} />
+        <input type="date" value={date} onChange={e => setDate(e.target.value)} />
+        <input placeholder="Weekly, Daily, Monthly?" value={frequency} onChange={e => setFrequency(e.target.value)} />
+        Completed?
+        <input type="checkbox" checked={completed} onChange={e => setCompleted(e.target.checked)} />
+        <button onClick={handleAdd}>Add Chore</button>
+      </p>
+      <h1>To-Do</h1>
+      <p>
+        {incompleteChores.map((item) => (
+          <li>{item.name} {item.date}</li>
+        ))}
+      </p>
+      <h1>Completed</h1>
+      <p>
+        {completedChores.map((item) => (
+          <li>{item.name} {item.date}</li>
+        ))}
+      </p>
     </div>
   );
 }
+
 
 export default App;
