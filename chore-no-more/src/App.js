d31c8fd219e4e2932c0914b37d98f5bdf73ba522
FrankieVara <fvarajr@gmail.com>
2023-04-01 14:00:16 -0700

    attempt to commit

--- chore-no-more/src/App.js
+++ chore-no-more/src/App.js
@@ -1,44 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { createClient } from '@supabase/supabase-js';
 import './App.css';
 
-//https://www.robinwieruch.de/react-add-item-to-list/
-
-import React from "react"
-
-const initialChores = [
-  {
-    name: "Test",
-    date: "2023-02-20",
-    completed: true,
-    frequency: "Weekly",
-  },
-];
+const supabaseUrl = "https://wqamrjqdnsnboscermtz.supabase.co";
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxYW1yanFkbnNuYm9zY2VybXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2MDU0MjQsImV4cCI6MTk5MzE4MTQyNH0.pI8ucJfxK3Qgx1oGoZRXfYpzIjoxwY2_BqXH0qls7jA';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const App = () => {
-  const [chores, setChores] = React.useState(initialChores);
-  const [name, setName] = React.useState('')
-  const [date, setDate] = React.useState('')
-  const [frequency, setFrequency] = React.useState('')
-  const completedChores = chores.filter(item => item.completed === true);
-  const incompleteChores = chores.filter(item => item.completed === false);
+  const [chores, setChores] = useState([]);
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+  const [frequency, setFrequency] = useState('');
 
-  function handleAdd () {
-      const newChore = {
-        name,
-        date,
-        completed: false,
-        frequency,
-      };
-      const newList = [...chores, newChore];
-      setChores(newList);
-      console.log(name, date, frequency, newChore.completed);
+  useEffect(() => {
+    supabase
+      .from('chores')
+      .select('*')
+      .then(({ data: chores, error }) => {
+        if (error) {
+          console.log(error);
+        } else {
+          setChores(chores);
+        }
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  function handleAdd() {
+    const newChore = {
+      name,
+      date,
+      completed: false,
+      frequency,
+    };
+    supabase
+      .from('chores')
+      .insert([newChore])
+      .then(({ data: chores, error }) => {
+        if (error) {
+          console.log(error);
+        } else {
+          setChores([...chores, newChore]);
+        }
+      })
+      .catch(error => console.error(error));
   }
 
   function handleComplete(index) {
     const updatedChores = [...chores];
-    updatedChores[index].completed = true;
-    setChores(updatedChores);
-    console.log(name, date, frequency, updatedChores[index].completed);
+    const chore = updatedChores[index];
+    chore.completed = true;
+    supabase
+      .from('chores')
+      .update({ completed: true })
+      .match({ id: chore.id })
+      .then(() => {
+        setChores(updatedChores);
+      })
+      .catch(error => console.error(error));
   }
+
+  const completedChores = chores.filter(item => item.completed === true);
+  const incompleteChores = chores.filter(item => item.completed === false);
 
   return (
     <div className="App">
