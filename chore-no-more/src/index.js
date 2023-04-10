67e4d01dfa5649799662941b89040f18255ecfdd
Eesha Gadhia <gadhiaeesha@gmail.com>
2023-04-09 18:08:52 -0700

    Revert "Calendar to tasks"

--- chore-no-more/src/index.js
+++ chore-no-more/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {supabase} from './supabaseClient'
+import { createClient} from '@supabase/supabase-js';
 import {SessionContextProvider} from '@supabase/auth-helpers-react';
 
+const supabase = createClient(
+  "https://wqamrjqdnsnboscermtz.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxYW1yanFkbnNuYm9zY2VybXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2MDU0MjQsImV4cCI6MTk5MzE4MTQyNH0.pI8ucJfxK3Qgx1oGoZRXfYpzIjoxwY2_BqXH0qls7jA"
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
