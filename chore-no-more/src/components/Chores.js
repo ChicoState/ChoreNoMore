651d15864caf29ced347194c291c0337aa423433
FrankieVara <fvarajr@gmail.com>
2023-05-01 13:29:55 -0700

    not deleting wihtout refreshing

--- chore-no-more/src/components/Chores.js
+++ chore-no-more/src/components/Chores.js
@@ -1,61 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { supabase } from '../supabaseClient';
 import { useSession } from '@supabase/auth-helpers-react';
 import { TaskChores } from './TaskChores';
 import { GoogleSignIn } from './GoogleSignIn';
-import  Chorecard  from './Chorecard';
-
-export function Chores() {
+export function Chores({groupId}) {
     const [ chores, setChoresList ] = useState([]);
     const [ choreName, setChoreName ] = useState('');
     const [ choresLoaded, setChoresLoaded ] = useState('');
+    const [ addedChores, setAddedChores ] = useState(false);
     const session = useSession();
-
-    async function insertChores(){
-        const {/*data ,*/ error} = await supabase.from('Chores')
-        .insert([{Chore: choreName, Created: session.user.email}]);
-        if(error){
-          console.log(error);
-        }else{
-          //console.log(data);
-          fetchChores();
-        }
-    }
-
-    async function fetchChores(){
-        //console.log("In fetchChores");
-        const {data , error} = await supabase
-        .from('Chores')
-        .select()
-        .eq('Created', session.user.email)
-
-        if(error){
+    const [ claimed, setClaimed ] = useState(false);
+    const [ deleted, setDeleted ] = useState(false);
+    useEffect(() => {
+      async function fetchChores(){
+          //console.log("In fetchChores");
+          const {data , error} = await supabase
+          .from('Chores')
+          .select()
+          .eq('Created', session.user.email)
+          if(error){
             console.log(error);
-        }else{
+          }else{
             //console.log(session.user.email)
             //console.log(data);
             setChoresList(data);
             setChoresLoaded(true);
-        }
-    }
-
-  async function deleteChores(){ 
-       const {data, error} = await supabase
-       .from('Chores')
-       .delete()
-       .eq('id', chore.id)
-       if (error) {
-         console.log(error)
-       }
-       if (data) {
-         console.log(data)
-       }
-     }
-   
+          }
+      }
     
-    if(choresLoaded !== true) {
-        fetchChores();
+      fetchChores();
+      setAddedChores(false);
+      setClaimed(false);
+      setDeleted(false);
+    }, [addedChores, groupId, choresLoaded, claimed]);
+    async function insertChores(){
+      const {/*data ,*/ error} = await supabase.from('Chores')
+      .insert([{Chore: choreName, Created: session.user.email, Group: groupId}]);
+      if(error){
+        console.log(error);
+      }else{
+        //console.log(data);
+        //fetchChores();
+        //console.log(addedChores)
+        setAddedChores(true);
+        //console.log(addedChores)
+      }
+
+  }
+  async function claimChore(chore){
+    //console.log(chore);
+    const{error} = await supabase
+    .from('Chores')
+    .update({Assignee : session.user.email})
+    .eq('id', chore)
+    if(error){
+      console.log(error)
+    }
+    setClaimed(true);
+  }
+
+  async function deleteChore(chore){
+      const { data, error } = await supabase
+        .from('Chores')
+        .delete()
+        .eq('id', chore)
+      
+      if (error) {
+        console.log(error)
+      }
+      if (data) {
+        console.log(data)
+      }
+      setDeleted(true);
     }
 
     return (
@@ -72,16 +89,7 @@ export function Chores() {
             }
             <h1>Incomplete Chores</h1>
             <div>
-            {chores && (
-        <div className="chores">
-          {/* order-by buttons */}
-          <div className="chore-grid">
-            {chores.map(chore => ( 
-              <Chorecard key={chore.id} chore={chore} />
-            ))}
-          </div>
-        </div>
-      )}
+             {chores.map(todo => <div><b>Chore:</b> {todo.Chore}<br></br>{todo.Assignee ? <div>User: {todo.Assignee}</div>:<button onClick= {()=> claimChore(todo.id)}>Claim</button>}:<button onClick = {()=> deleteChore(todo.id)}>Complete Chore</button></div>)}
             </div>
         </div>
     );
